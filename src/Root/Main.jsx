24109e40e860243efd23999621a21232623c322e
Muhammad Nakib Uddin <fliptechofficial@gmail.com>
2026-02-22 08:19:59 +0000

    creating components

---
+++ src/Root/Main.jsx
@@ -0,0 +1,12 @@
+import Header from "../Components/Header";
+
+const Main = () => {
+  return (
+    <div className="">
+      <Header/>
+      
+    </div>
+  );
+};
+
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Main from './Root/Main.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Main/>
   </StrictMode>,
 )
